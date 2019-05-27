b1dc96eaa5ee5ec3bd9c60903d2959d3c67e3cff
Ács Bence Tamás <acs.bence.tamas@gmail.com>
2019-05-27 11:41:46 +0200

    Basic logic behind bets

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,7 +3,7 @@ package org.leanpoker.player;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
-import java.util.Map;
+import java.util.*;
 
 public class Player {
 
@@ -12,9 +12,32 @@ public class Player {
     public static int betRequest(JsonElement request) {
         Gson gson = new Gson();
         GameState gameState = gson.fromJson(request,GameState.class);
-        return gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise + 10;
+        int defaultBet = gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise;
+        if (anythingGood(gameState)) {
+            return defaultBet + 50;
+        } else {
+            return defaultBet + 10;
+        }
     }
 
     public static void showdown(JsonElement game) {
+    }
+
+    private static boolean anythingGood(GameState gameState) {
+        List<Card> relevantCards = new ArrayList<>();
+        for (Card card:gameState.community_cards) {
+            if (card != null) {
+                relevantCards.add(card);
+            }
+        }
+        relevantCards.addAll(Arrays.asList(gameState.players[gameState.in_action].hole_cards));
+        Map<String, Integer> ranks = new HashMap<>();
+        for (Card card : relevantCards) {
+            ranks.put(card.rank, ranks.getOrDefault(card.rank, 0) + 1);
+        }
+        for (Integer count : ranks.values()) {
+            if (count > 1) return true;
+        }
+        return false;
     }
 }
